59820436c68d691ae81707c0391c539375f44f3e
pragatikavra28 <pragati.kavra_cs23@gla.ac.in>
2025-08-13 12:35:14 +0530

    added: java fundametals practice problem

---
+++ java-fundamentals/addtwonumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class addtwonumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Sum = " + (num1 + num2));
+        sc.close();
+    }
+}
+
